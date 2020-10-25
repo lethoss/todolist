fc43afbb90bb8bf814a101be70cad1536d011460
Andrii Melnyk <lethoss@ukr.net>
2020-10-25 22:13:27 +0200

    deploy

---
+++ precache-manifest.21027ae42a642cc4e61142883995f3f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4894de2c2d4a1341ce",
+    "url": "/todolist/css/app.e7c244fe.css"
+  },
+  {
+    "revision": "4dcab06530eb22ceaca0",
+    "url": "/todolist/css/chunk-6daa5491.7e948051.css"
+  },
+  {
+    "revision": "5f60a2743848f3be4b9d3b060308e51c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8a4894de2c2d4a1341ce",
+    "url": "/todolist/js/app.e384f230.js"
+  },
+  {
+    "revision": "4dcab06530eb22ceaca0",
+    "url": "/todolist/js/chunk-6daa5491.e15faf38.js"
+  },
+  {
+    "revision": "bef4fab6f2efc2ab3247",
+    "url": "/todolist/js/chunk-vendors.baab520f.js"
+  },
+  {
+    "revision": "33f7fa286bf61f74c8793938f3d7d99b",
+    "url": "/todolist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todolist/robots.txt"
+  }
+]);
